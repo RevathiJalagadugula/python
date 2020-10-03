df0797e3db21303e8b30bf4dcd95ca24aebe4a93
RevathiJalagadugula <71430776+RevathiJalagadugula@users.noreply.github.com>
2020-10-03 12:48:40 +0530

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,21 +1,4 @@
-#def greet():
-#	print('good morning')
-#	print('welcome to python class')
-#greet()
-
-#def add(x,y):
-#	c=x+y
-#	print(c)
-#add(5,6)
-
-#def human(name,age):
-#	print(name)
-#	print(age)
-#human('Revathi',18)
-
-#def add(x,*y):
-#	c=x
-#	for i in y:
-#		c+=i
-#	print(c)
-#add(100,200,300,400,500,600,700,00,900,1000)
+def greet():
+	print('good morning')
+	print('welcome to python')
+greet()
