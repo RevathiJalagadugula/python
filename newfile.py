fc52f569dd9f92aac1f4972b9adc44dc65c61042
RevathiJalagadugula <71430776+RevathiJalagadugula@users.noreply.github.com>
2020-10-03 20:47:20 +0530

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,6 +1,16 @@
-x=int(input('enter a number'))
-y=int(input('enter a number'))
-x=x%y
-x=x%y
-y=y%x
-print(y)
+def add(x,y):
+	c=x+y
+	print(c)
+add(5,6)
+
+def human(name,age):
+	print(name)
+	print(age)
+human('Revathi',18)
+
+def add(x,*y):
+	c=x
+	for i in y:
+		c+=i
+	print(c)
+add(100,200,300,400,500,600,700,00,900,1000)
