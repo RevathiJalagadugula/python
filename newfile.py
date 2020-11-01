bc35a308914a4ce95c5f08ec6560a06d30278840
RevathiJalagadugula <71430776+RevathiJalagadugula@users.noreply.github.com>
2020-11-01 10:21:54 +0530

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,5 +1,11 @@
 n=int(input('enter a number'))
-fact=1
-for i in range(n,0,-1):
-	fact*=i
-print(fact)
+a=n
+s=0
+while n>0:
+	rem=n%10
+	s+=rem
+	n//=10
+if a%s==0:
+	print(a,'is a harshed number')
+else:
+	print(a,'is not a harshed number')
